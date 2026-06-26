a6b03a2a8510bd97319601c811b1ae3f03302417
Gustavo Fernandes <gutof.carvalho@gmail.com>
2026-06-26 16:04:33 -0300

    adding the logs status and the OS status of the  hackers and serfs

--- visualizador.py
+++ visualizador.py
@@ -5,11 +5,11 @@ import queue
 import time
 import math
 
-# dimensoes da tela
+# dimensions of the game
 SCREEN_WIDTH = 1100
 SCREEN_HEIGHT = 650
 
-# rgb do grafico
+# graphics
 COLOR_BG = (18, 24, 38)          # background
 COLOR_RIVER_BED = (26, 36, 56)    # Fundo do rio
 COLOR_RIVER = (0, 180, 216)       # \água
@@ -20,19 +20,19 @@ COLOR_BOAT_DARK = (141, 91, 35)   # barco sombra
 COLOR_TEXT_MAIN = (248, 249, 250) # texto
 COLOR_TEXT_MUTED = (108, 117, 125)# texto 2   mudar isso dps  ta feio
 
-# iniciando
+# Inicializando o jogo
 pygame.init()
-pygame.display.set_caption("MC504 - Multithread River Crossing Visualizer")
+pygame.display.set_caption("MC504 - Multithread River Crossing Visualizer") #title
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-# Fontes mais elegantes
+# Fonts
 font_title = pygame.font.SysFont("Segoe UI", 32, bold=True)
 font_bank = pygame.font.SysFont("Segoe UI", 22, bold=True)
 font_status = pygame.font.SysFont("Consolas", 16, bold=True)
 font_id = pygame.font.SysFont("Segoe UI", 14, bold=True)
 
-# filas
+# Queue
 hacker_queue_list = []
 serf_queue_list = []
 boat_list = []
@@ -43,11 +43,25 @@ animation_speed = 2
 input_command_queue = queue.Queue()
 wave_offset = 0 # p animar a correnteza do rio
 
-# classes Hackers e Serfs
+sys_state = {
+    "mutex":"UNLOCKED",
+    "barrier": "CLOSED"
+}
+#logs
+event_logs = []
+max = 6
+font_log = pygame.font.SysFont("Consolas", 13, bold=False)
+def add_log(message, color=COLOR_TEXT_MAIN):
+    curr_time = time.strftime("%H:%M:%S")
+    event_logs.append((f"{curr_time} {message}", color))
+    if (len(event_logs) > max):
+        event_logs.pop(0)
+# Entity Class
 class Entity:
     def __init__(self, id, type, current_x, current_y):
         self.id = id
-        self.type = type # 'HACKER' ou 'SERF'
+        self.type = type # HACKER ou SERF
+        #position 
         self.x = current_x
         self.y = current_y
         self.radius = 16
@@ -60,17 +74,16 @@ class Entity:
         pulse_radius = self.radius + int(math.sin(self.pulse) * 3)
         base_color = COLOR_HACKER if self.type == 'HACKER' else COLOR_SERF
         
-        # efeito de brilho neon ao redor
+        # shine effect
         glow_surface = pygame.Surface((pulse_radius * 2, pulse_radius * 2), pygame.SRCALPHA)
         pygame.draw.circle(glow_surface, (*base_color, 60), (pulse_radius, pulse_radius), pulse_radius)
         screen.blit(glow_surface, (int(self.x - pulse_radius), int(self.y - pulse_radius)))
         
-        #  corpo principal da entidade
+        # body of the entity
         pygame.draw.circle(screen, base_color, (int(self.x), int(self.y)), self.radius)
         pygame.draw.circle(screen, COLOR_BG, (int(self.x), int(self.y)), self.radius - 3)
         pygame.draw.circle(screen, base_color, (int(self.x), int(self.y)), self.radius - 6)
         
-        #  renderiza o ID centralizado
         id_surf = font_id.render(f"{'H' if self.type == 'HACKER' else 'S'}{self.id}", True, COLOR_TEXT_MAIN)
         id_rect = id_surf.get_rect(center=(self.x, self.y))
         screen.blit(id_surf, id_rect)
@@ -80,18 +93,18 @@ class Entity:
         self.x += (self.target_x - self.x) * 0.12
         self.y += (self.target_y - self.y) * 0.12
 
-#renderização do cenário 
+#render the scenario
 def draw_environment():
     screen.fill(COLOR_BG)
     global wave_offset
     wave_offset += 0.5
     
-    # desenha barquinho
+    # Draw the boat
     river_x, river_width = 350, 400
     pygame.draw.rect(screen, COLOR_RIVER_BED, (river_x, 0, river_width, SCREEN_HEIGHT))
     pygame.draw.rect(screen, COLOR_RIVER, (river_x + 10, 0, river_width - 20, SCREEN_HEIGHT))
     
-    # aanimação linhas da correnteza 
+    # animate the water - "waves"
     for y in range(0, SCREEN_HEIGHT, 40):
         y_pos = (y + int(wave_offset)) % SCREEN_HEIGHT
         pygame.draw.line(screen, (255, 255, 255, 40), (river_x + 50, y_pos), (river_x + 80, y_pos + 10), 2)
@@ -111,6 +124,43 @@ def draw_environment():
     
     screen.blit(font_bank.render("LEFT BANK", True, COLOR_TEXT_MAIN), (50, 140))
     screen.blit(font_bank.render("RIGHT BANK", True, COLOR_TEXT_MAIN), (SCREEN_WIDTH - 200, 140))
+
+def draw_system():
+    width = 2
+    height = 110
+    surface = pygame.Surface((width, height), pygame.SRCALPHA)
+    pygame.draw.rect(surface, (10, 15, 25, 210), (0, 0, width, height), border_radius=8)
+    pygame.draw.rect(surface, (60, 70, 90, 255), (0, 0, width, height), width=2, border_radius=8)
+
+    begin_x, begin_y = SCREEN_WIDTH - 300,20
+    screen.blit(surface, (begin_x, begin_y))
+    screen.blit(font_status.render("Operational System State", True, (255,215,0)), (begin_x + 15, begin_y + 15))
+
+    mutex_color = COLOR_SERF if sys_state["mutex"] == "LOCKED" else COLOR_HACKER
+    screen.blit(font_status.render(f"Mutex:   {sys_state['mutex']}", True, mutex_color), (begin_x + 15, begin_y + 45))
+
+    barrier_color = COLOR_SERF if sys_state["barrier"] == "CLOSED" else COLOR_HACKER
+    screen.blit(font_status.render(f"Barrier: {sys_state['barrier']}", True, barrier_color), (begin_x + 15, begin_y + 70))
+
+def draw_logs():
+    width = 420
+    height = 150
+    begin_x = 50
+    begin_y = SCREEN_HEIGHT - height - 10
+
+    surface = pygame.Surface((width, height), pygame.SRCALPHA)
+    pygame.draw.rect(surface, (10,15,25,220), (0,0,width,height), border_radius=6)
+    pygame.draw.rect(surface, (60, 70, 90, 255), (0, 0, width, height), width=1, border_radius=6)
+    screen.blit(surface, (begin_x, begin_y))
+
+    screen.blit(font_status.render("OS Event Log", True, COLOR_TEXT_MUTED), (begin_x + 10, begin_y + 8))
+    pygame.draw.line(screen, (60, 70, 90), (begin_x + 10, begin_y + 28), (begin_x + width - 10, begin_y + 28))
+
+    for i, (msg, color) in enumerate(event_logs):
+        surf = font_log.render(msg, True, color)
+        screen.blit(surf, (begin_x + 10, begin_y + 35 + (i * 17)))
+
+
 
 def draw_boat_and_passengers():
     boat_width = 190
@@ -227,6 +277,9 @@ while running:
                     hacker_queue_list.append(new_entity)
                 else:
                     serf_queue_list.append(new_entity)
+                
+                cor = COLOR_HACKER if p_type == 'HACKER' else COLOR_SERF
+                add_log(f"Thread created: {p_type} {idx} waiting.", cor)
                     
             elif action == "EMBARCOU":
                 if p_type == "HACKER":
@@ -242,10 +295,19 @@ while running:
                             boat_list.append(ent)
                             break
                 boat_status_text = f"PASSENGERS BOARDING ({len(boat_list)}/4)"
+                add_log(f"Signal recieved: {p_type} {idx} joined to the boat.", COLOR_TEXT_MAIN)
                 
             elif action == "REMOU":
                 boat_status_text = f"CRUISING - CAPTAIN: {p_type} {idx} ROWING"
                 boat_is_rowing = True
+                add_log(f"Travel started (Leader: {p_type} {idx}).", (255, 215, 0))
+
+            elif action == "STATUS":
+                key = parts[1].lower()
+                value = parts[2].upper()
+                if key in sys_state:
+                    sys_state[key] = value 
+                add_log(f"OS Kernel -> {key.upper()} changed to {value}", COLOR_TEXT_MUTED)
 
     except queue.Empty:
         pass
@@ -269,6 +331,8 @@ while running:
     draw_environment()
     organize_waiting_queues()
     draw_boat_and_passengers()
+    draw_system()
+    draw_logs()
     
     pygame.display.flip()
     clock.tick(60)
